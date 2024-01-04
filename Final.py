b2a667a948c02d2d9f19d4b054d0dfb6e26686c6
sw8744 <sw8744@naver.com>
2024-01-04 16:05:24 +0900

    Finish Final.py

--- Final.py
+++ Final.py
@@ -55,18 +55,37 @@ swarm.parallel(lambda i, tello: tello.move_back(150) if i % 2 == 1 else tello.mo
 swarm.land()
 
 # Legends Never Die
+while True:
+    input_value = input('press t to takeoff Drone #1, #5, #9 : ')
+    if input_value == 't':
+        break
+    else:
+        continue
+
 j = 1
 for i in swarm:
     if j in [1, 5, 9]:
         i.takeoff()
     j += 1
 
+while True:
+    input_value = input('press t to takeoff Drone #2, #6, #10: ')
+    if input_value == 't':
+        break
+    else:
+        continue
 j = 1
 for i in swarm:
     if j in [2, 6, 10]:
         i.takeoff()
     j += 1
 
+while True:
+    input_value = input('press t to takeoff Drone #3, #4, #7, #8: ')
+    if input_value == 't':
+        break
+    else:
+        continue
 j = 1
 for i in swarm:
     if j in [3, 4, 7, 8]:
@@ -77,17 +96,97 @@ swarm.parallel(lambda i, tello: tello.move_up(300))
 j = 1
 for i in swarm:
     if j in [1, 9]:
-        i.move_back(30)
+        i.move_forward(150)
     elif j in [2, 10]:
-        i.move_forward(30)
-    elif j in [3, 7]:
-        i.move_forward(70)
-    elif j in [4, 8]:
-        i.move_back(70)
+        i.move_back(300)
     elif j in [5]:
-        i.move_forward(170)
+        i.move_forward(132)
     elif j in [6]:
-        i.move_back(170)
+        i.move_back(132)
     j += 1
 
+swarm.parallel(lambda i, tello: tello.move_down(200) if i in [3, 4, 5, 6, 7, 8] else tello.move_down(0))
+j = 1
+for i in swarm:
+    if j in [2, 10]:
+        i.move_forward(300)
+    elif j in [1, 9]:
+        i.move_back(150)
+    elif j in [4, 8]:
+        i.move_forward(50)
+    elif j in [5]:
+        i.move_back(132)
+    j += 1
+
+j = 1
+for i in swarm:
+    if j in [6]:
+        i.move_up(300)
+    elif j in [1, 2, 9, 10]:
+        i.move_down(300)
+    elif j in [3, 4, 5, 7, 8]:
+        i.move_down(100)
+    j += 1
+
+swarm.parallel(lambda i, tello: tello.flip('f') if i in [6] else tello.move_back(0))
+swarm.land()
+
+# GODS
+while True:
+    input_value = input('press t to takeoff Drone #1, #10 : ')
+    if input_value == 't':
+        break
+    else:
+        continue
+
+j = 1
+for i in swarm:
+    if j in [1, 10]:
+        i.takeoff()
+    j += 1
+
+while True:
+    input_value = input('press t to takeoff Drone #2, #3, #8, #9 : ')
+    if input_value == 't':
+        break
+    else:
+        continue
+j = 1
+for i in swarm:
+    if j in [2, 3, 8, 9]:
+        i.takeoff()
+    j += 1
+
+while True:
+    input_value = input('press t to takeoff Drone #4, #5, #6, #7: ')
+    if input_value == 't':
+        break
+    else:
+        continue
+j = 1
+for i in swarm:
+    if j in [4, 5, 6, 7]:
+        i.takeoff()
+    j += 1
+
+swarm.parallel(lambda i, tello: tello.move_up(200))
+# FIXME: 파도타기 추가
+j = 1
+for i in swarm:
+    if j in [2, 8]:
+        i.move_back(73)
+    elif j in [3, 9]:
+        i.move_forward(73)
+    elif j in [4, 6]:
+        i.move_back(150)
+    elif j in [5, 7]:
+        i.move_forward(150)
+    elif j in [10]:
+        i.move_left(100)
+    elif j in [2]:
+        i.move_right(100)
+    j += 1
+swarm.parallel(lambda i, tello: tello.move_down(200) if i in [1, 4, 5, 8, 9] else tello.move_down(0))
+swarm.parallel(lambda i, tello: tello.flip('f'))
+swarm.land()
 swarm.end()
